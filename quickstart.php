2bd63a4fa7b739350763d0548f3bfa7c43c31129
Jason <jason@hidalgo>
2012-08-16 18:59:09 -0500

    adding a quickstart init file

---
+++ quickstart.php
@@ -0,0 +1,17 @@
+<?php
+/**
+*	get all of the includes required
+*/
+$cwd = getcwd ();
+chdir (dirname (__FILE__));
+
+require_once 'lib/autoloader/application/AutoLoader.php';
+require_once 'lib/nicedog/NiceDog.php';
+
+chdir ($cwd);
+
+
+function quickstart_init ($autoLoadDir)
+{
+	AutoLoader::instance ($autoLoadDir);
+}
